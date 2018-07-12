18d37029255a2b22524ca41e5c422d3039760e8c
Tim Biles <timbilestimbiles@gmail.com>
2018-07-12 13:44:43 -0500

    finished

--- constructors.js
+++ constructors.js
@@ -13,9 +13,10 @@
   The values for the make and model will come from two parameters, make and model (in that order).
 */
 
-// Code here
-
-
+function CarFactory(make, model) {
+  this.make = make;
+  this.model = model;
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -32,9 +33,7 @@ function Employee(name, email, hireDate) {
   Assign the result of the invocation to a variable called bob.
 */
 
-// Code here
-
-
+const bob = new Employee('Bob', 'bob@gmail.com', '01-02-98');
 
 ////////// PROBLEM 4 //////////
 
@@ -53,6 +52,17 @@ mustang.moveCar(); // Increments mustang' move property by 10. Returns the new m
   You'll also need to use the 'this' keyword properly in order to make sure you're invoking moveCar on the right object (prius vs mustang).
 */
 
-// Code here
+function Car(make, model, year) {
+  let move = 0;
+  
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  this.moveCar = function(){
+    return move += 10;
+  }
+}
 
-
+// Car.prototype.moveCar = function() {
+//   return (this.move += 10);
+// };
